e53f3efea8d0a4f0614a6085c17c94c8732824ea
David Young <david@textflex.com>
2018-10-16 09:52:44 +0800

    Extend mouse drag marker update to pairs markers
    Incorporate token marker changes to show text selection during mouse motion events to pairs markers.

--- src/main/java/jsyntaxpane/components/PairsMarker.java
+++ src/main/java/jsyntaxpane/components/PairsMarker.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2018 David Young david@textflex.com
+ * based on original file by
  * Copyright 2008 Ayman Al-Sairafi ayman.alsairafi@gmail.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
@@ -16,6 +18,8 @@ package jsyntaxpane.components;
 import java.awt.Color;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import jsyntaxpane.actions.*;
 import javax.swing.JEditorPane;
 import javax.swing.event.CaretEvent;
@@ -31,7 +35,8 @@ import jsyntaxpane.util.Configuration;
  *
  * @author Ayman Al-Sairafi
  */
-public class PairsMarker implements CaretListener, SyntaxComponent, PropertyChangeListener {
+public class PairsMarker implements CaretListener, SyntaxComponent, 
+		PropertyChangeListener, MouseMotionListener {
 
     public static final String PROPERTY_COLOR = "PairMarker.Color";
     private JTextComponent pane;
@@ -40,11 +45,33 @@ public class PairsMarker implements CaretListener, SyntaxComponent, PropertyChan
 
     public PairsMarker() {
     }
-
+    
+    /**
+     * Update marker after caret update.
+     * @param e caret event to find the caret position
+     */
     @Override
     public void caretUpdate(CaretEvent e) {
+        // will not fire until after mouse selection completed
+        markTokenAt(e.getDot());
+    }
+
+    /**
+     * Update marker at start of selection, which will remove any existing 
+     * marker there to avoid masking selection highlight by marker highlight.
+     * @param e mouse event, not used
+     */
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        markTokenAt(pane.getSelectionStart());
+    }
+    
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+    
+    public void markTokenAt(int pos) {
         removeMarkers();
-        int pos = e.getDot();
         SyntaxDocument doc = ActionUtils.getSyntaxDocument(pane);
         Token token = doc.getTokenAt(pos);
         if (token != null && token.pairValue != 0) {
@@ -74,6 +101,7 @@ public class PairsMarker implements CaretListener, SyntaxComponent, PropertyChan
     public void install(JEditorPane editor) {
         pane = editor;
         pane.addCaretListener(this);
+        pane.addMouseMotionListener(this);
         status = Status.INSTALLING;
     }
 
@@ -81,6 +109,7 @@ public class PairsMarker implements CaretListener, SyntaxComponent, PropertyChan
     public void deinstall(JEditorPane editor) {
         status = Status.DEINSTALLING;
         pane.removeCaretListener(this);
+        pane.removeMouseMotionListener(this);
         removeMarkers();
     }
 
@@ -88,8 +117,10 @@ public class PairsMarker implements CaretListener, SyntaxComponent, PropertyChan
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("document")) {
                 pane.removeCaretListener(this);
+        		pane.removeMouseMotionListener(this);
             if (status.equals(Status.INSTALLING)) {
                 pane.addCaretListener(this);
+                pane.addMouseMotionListener(this);
                 removeMarkers();
             }
         }
